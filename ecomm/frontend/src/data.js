db5e2be1a1b7b050cce542a0147df498c07dd1a0
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-24 09:46:00 -0300

    Create frontend

---
+++ ecomm/frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+  products:[
+    {
+      _id:'1',
+      name:'Camisa Nike',
+      category:'Camisas',
+      image:'/images/p1.jpg',
+      price: 120,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'produto de alta qualidade'
+    },
+    {
+      _id:'2',
+      name:'Camisa Nike Polo',
+      category:'Camisas',
+      image:'/images/p2.jpg',
+      price: 130,
+      brand: 'Nike',
+      rating: 4.6,
+      numReviews: 15,
+      description: 'produto de alta qualidade'
+    },
+    {
+      _id:'3',
+      name:'Camisa Nike Polo',
+      category:'Camisas',
+      image:'/images/p3.jpg',
+      price: 140,
+      brand: 'Nike',
+      rating: 4.7,
+      numReviews: 20,
+      description: 'produto de alta qualidade'
+    },
+    {
+      _id:'4',
+      name:'Calça Jeans',
+      category:'Calças',
+      image:'/images/p4.jpg',
+      price: 150,
+      brand: 'Nike',
+      rating: 4.8,
+      numReviews: 25,
+      description: 'produto de alta qualidade'
+    },
+    {
+      _id:'5',
+      name:'Calça Social',
+      category:'Calças',
+      image:'/images/p5.jpg',
+      price: 100,
+      brand: 'Adidas',
+      rating: 4.8,
+      numReviews: 30,
+      description: 'produto de alta qualidade'
+    },
+    {
+      _id:'6',
+      name:'Calça Social',
+      category:'Calças',
+      image:'/images/p6.jpg',
+      price: 90,
+      brand: 'Puma',
+      rating: 4.9,
+      numReviews: 30,
+      description: 'produto de alta qualidade'
+    },
+  ],
+};
+
+export default data;
