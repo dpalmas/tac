cd528bf89df7c40512369f1558b11270f5a8d4d9
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-26 15:39:22 -0300

    Update ecomm

--- ecomm/frontend/src/App.js
+++ ecomm/frontend/src/App.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="/">
-            amazona
-          </a>
+          <Link className="brand" to="/">
+            ecomm
+          </Link>
         </div>
         <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
+        <Link to="/cart">
+              Carrinho
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Entrar</Link>
+          </div>
           </header>
         <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
-        <footer className="row center">All right reserved</footer>
+        <footer className="row center">Todos os direitos reservados</footer>
       </div>
     </BrowserRouter>
   );
